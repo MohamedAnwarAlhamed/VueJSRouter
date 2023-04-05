03b6b9d5245b1b94f551469fd818d731bc94d4c7
Mohamed Anwar <76563640+MohamedAnwarAlhamed@users.noreply.github.com>
2023-04-05 23:57:06 +0300

    Router 1

---
+++ my_app/src/views/ContactView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Contact</h1>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+
+
+<script lang="ts">
+    export default {
+        name: 'ContactView',
+        data() {
+            return {
+                message: 'Hello from ContactView'
+            }
+        }
+    }
+</script>
+
+<style></style>
