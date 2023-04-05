4b06df7f1843cc92237a695954a73cbd8f02a32f
Mohamed Anwar <76563640+MohamedAnwarAlhamed@users.noreply.github.com>
2023-04-06 02:24:55 +0300

    Router 4

--- my_app/src/views/UserView.vue
+++ my_app/src/views/UserView.vue
@@ -2,18 +2,36 @@
     <div>
         <h1>User</h1>
         <p>{{ messages }}</p>
-        <p>{{ $route.params.id }}</p>
-        <p>{{ $route.params.name }}</p>
+        <ul>
+            <li v-for="item in list" :key="item.id">
+                {{ item.name }}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script lang="ts">
+import * as Vue from 'vue'
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+Vue.use(VueAxios, axios);
     export default {
         name: 'UserView',
         data() {
             return {
-                messages: 'Hello from UserView'
+                messages: 'Hello from UserView',
+                list:undefined
             }
+        },
+        created(){
+            vue.axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                this.list = response.data;
+            })
+            .catch(error => {
+                console.log(error);
+            })
         }
     }
 
